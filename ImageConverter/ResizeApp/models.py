ce0b3c943a17fa3b1ed2a432d1b31a0489988cae
Blati <blati1996@gmail.com>
2021-01-28 21:00:08 +0300

    Image Model addded

--- ImageConverter/ResizeApp/models.py
+++ ImageConverter/ResizeApp/models.py
@@ -1,3 +1,21 @@
+import os
+import urllib.request
+
+from django.core.files import File
 from django.db import models
 
-# Create your models here.
+
+class Document(models.Model):
+    image = models.URLField(max_length=255)
+    link = models.FileField(verbose_name="Файл", blank=True)
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        if self.image and not self.link:
+            result = urllib.request.urlretrieve(self.image)
+            self.link.save(
+                os.path.basename(self.image),
+                File(open(result[0], 'rb'))
+            )
+            self.save()
+        super().save(*args, **kwargs)
