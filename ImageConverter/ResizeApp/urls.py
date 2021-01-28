3ce076e93de349a42ec3267e846b1cdb04990391
Blati <blati1996@gmail.com>
2021-01-28 21:16:11 +0300

    Adding Page added

--- ImageConverter/ResizeApp/urls.py
+++ ImageConverter/ResizeApp/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from ResizeApp.views import home_view
+from ResizeApp.views import home_view, upload_image
 
 urlpatterns = [
     path('', home_view, name='home'),
+    path('add/', upload_image, name='add'),
 ]
--- ImageConverter/ResizeApp/views.py
+++ ImageConverter/ResizeApp/views.py
@@ -1,6 +1,31 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import render, redirect
 
+from ResizeApp.forms import ImageForm
 from ResizeApp.models import Image
+
+
+def upload_image(request):
+    if request.method == 'POST':
+        form = ImageForm(request.POST, request.FILES)
+        if form.is_valid():
+            if not form.cleaned_data['link'] and not form.cleaned_data['image']:
+                messages.add_message(request,
+                                     messages.ERROR,
+                                     "Введите хотя бы одно поле!")
+                return redirect('/')
+            if form.cleaned_data['link'] and form.cleaned_data['image']:
+                messages.add_message(request,
+                                     messages.ERROR,
+                                     "Введите хотя бы одно поле!")
+                return redirect('/')
+            form.save()
+            return redirect('/')
+    else:
+        form = ImageForm()
+    return render(request,
+                  'add.html',
+                  {'form': form})
 
 
 def home_view(request):
