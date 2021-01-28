95e98cf9d6035cd3b669416501b1baea5c590d57
Blati <blati1996@gmail.com>
2021-01-28 21:27:29 +0300

    Image detail page added

--- ImageConverter/ResizeApp/forms.py
+++ ImageConverter/ResizeApp/forms.py
@@ -7,3 +7,8 @@ class ImageForm(forms.ModelForm):
     class Meta:
         model = Image
         fields = ('link', 'image',)
+
+
+class UserForm(forms.Form):
+    width = forms.CharField(max_length=5, label='Ширина', required=False)
+    height = forms.CharField(max_length=5, label='Высота', required=False)
