c1478b89afafabc88bbd6994aab8bf817bd42031
Blati <blati1996@gmail.com>
2021-01-28 22:10:36 +0300

    Some docs added

--- ImageConverter/ResizeApp/utils.py
+++ ImageConverter/ResizeApp/utils.py
@@ -5,16 +5,33 @@ from PIL import Image
 
 
 def gen_name():
+    """
+        Функция генерации случайной последовательности букв для формирования уникального имени изображений
+        Возвращает строку вида: '_aBcDeF'
+    """
     res = '_' + ''.join(random.choice(string.ascii_letters) for x in range(5)) + '.'
     return res
 
 
 def get_path(raw_path):
+    """
+        Функция получения пути, имени и расширения для конкретного файла
+        Получает строку вида: '/media/googlelogo.png'
+        Возвращает строку вида: 'media\googlelogo_FEJjl.png'
+    """
     res = raw_path.split('.')[0] + gen_name() + raw_path.split('.')[1]
     return res.replace("/", "\\")[1:len(res)]
 
 
 def reformat_image(img_path, path, *args):
+    """
+        Функция скалирует изображение на указанное число пикселей, сохраняя пропорции, потом сохраняет его
+        Входные данные:
+        img_path -- путь к изображению
+        path -- путь для сохранения изображения
+        args -- ширина и длина
+        Возвращает 'OK' в случае успеха и None в случае неверно введённых данных
+    """
     img = Image.open(img_path)
 
     try:
