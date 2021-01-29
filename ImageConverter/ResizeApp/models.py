ea6cdc1b2abfd41af2ddb12f43dac25fa4aa31b7
Blati <blati1996@gmail.com>
2021-01-29 15:57:08 +0300

    Forgot to add default values in user input

--- ImageConverter/ResizeApp/models.py
+++ ImageConverter/ResizeApp/models.py
@@ -7,7 +7,7 @@ from django.db import models
 
 class Image(models.Model):
     link = models.URLField(verbose_name="Ссылка", max_length=255, blank=True)
-    image = models.FileField(verbose_name="Файл", blank=True)
+    image = models.ImageField(verbose_name="Файл", blank=True)
     uploaded_at = models.DateTimeField(auto_now_add=True)
 
     def save(self, *args, **kwargs):
